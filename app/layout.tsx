4c0b218a833d7e48e97b409f2a897002fdbc38df
priyanshu73 <pyakurelpriyanshu73@gmail.com>
2025-04-23 02:27:10 +0200

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const jetbrainsMono = JetBrains_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "Terminal Portfolio",
+  title: "Priyanshu Pyakurel Portfolio",
   description: "A terminal-themed portfolio website",
     generator: ''
 }
